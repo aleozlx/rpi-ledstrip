e4f4bacd6318f2a192938ac45eabc6c7c4a9f8e2
Alex Yang <aleozlx@gmail.com>
2021-11-24 22:44:03 -0800

    +udp

--- src/main.c
+++ src/main.c
@@ -45,6 +45,10 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <pthread.h>
+#ifdef SOCK_UDP
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#endif
 
 #include "clk.h"
 #include "gpio.h"
@@ -302,40 +306,69 @@ void parseargs(int argc, char **argv, ws2811_t *ws2811)
 	}
 }
 
+#ifndef SOCK_UDP
 const char* socket_path = "./ledstrip.sock";
+#endif
 char buf_sock[512];
 void* task_sock(void *arg) {
-    int client, rr;
-    struct sockaddr_un addr;
-    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    int rr;
+#ifndef SOCK_UDP
+    int client;
+    struct sockaddr_un
+#else
+    struct sockaddr_in
+#endif
+        addr;
+
+    sockfd = socket(
+#ifndef SOCK_UDP
+        AF_UNIX, SOCK_STREAM,
+#else
+        AF_INET, SOCK_DGRAM,
+#endif
+    0);
+
     if (sockfd < 0) {
         perror("ERROR opening socket");
         exit(EXIT_FAILURE);
     }
     memset(&addr, 0, sizeof(addr));
+#ifndef SOCK_UDP
     addr.sun_family = AF_UNIX;
     strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
     unlink(socket_path);
+#else
+    addr.sin_family      = AF_INET;  // IPv4
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port        = htons(5151);
+#endif
 
     if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
         perror("ERROR bind error");
         exit(EXIT_FAILURE);
     }
+#ifndef SOCK_UDP
     if (listen(sockfd, 5) == -1) {
         perror("ERROR listen error");
         exit(EXIT_FAILURE);
     }
+#endif
     while (running) {
+#ifndef SOCK_UDP
         if ((client = accept(sockfd, NULL, NULL)) == -1) {
             perror("ERROR accept error");
             continue;
         }
         rr = read(client, buf_sock, 4);
+#else
+        rr = recvfrom(sockfd, buf_sock, 4, MSG_PEEK, NULL, NULL);
+#endif
         if (-1 == rr) {
             perror("ERROR socket read");
         }
         if (0xCC == buf_sock[0] && buf_sock[0] == buf_sock[1]) {
             int ct = buf_sock[2] | (buf_sock[3] << 8);
+#ifndef SOCK_UDP
             if(pthread_mutex_lock(&mut) != 0) {
                 perror("ERROR mutex lock");
             }
@@ -343,23 +376,41 @@ void* task_sock(void *arg) {
             if(pthread_mutex_unlock(&mut) !=0) {
                 perror("ERROR mutex unlock");
             }
+#else
+            rr = recvfrom(sockfd, buf_sock, 4 + ct * sizeof(ws2811_led_t), MSG_WAITALL, NULL, NULL);
+            if(pthread_mutex_lock(&mut) != 0) {
+                perror("ERROR mutex lock");
+            }
+            memcpy(matrix, buf_sock+4, ct * sizeof(ws2811_led_t));
+            if(pthread_mutex_unlock(&mut) !=0) {
+                perror("ERROR mutex unlock");
+            }
+#endif
             if (-1 == rr) {
                 perror("ERROR socket read");
             }
         }
         else {
+#ifndef SOCK_UDP
             do {
                 rr = read(client, buf_sock, sizeof(buf_sock));
             } while (rr != 0 && rr != -1);
+#else
+            recvfrom(sockfd, buf_sock, sizeof(buf_sock), MSG_WAITALL, NULL, NULL);
+#endif
         }
+#ifndef SOCK_UDP
         close(client);
+#endif
     }
     if (sockfd != -1) {
         shutdown(sockfd, SHUT_RD);
         close(sockfd);
         sockfd = -1;
     }
+#ifndef SOCK_UDP
     unlink(socket_path);
+#endif
 }
 
 int main(int argc, char *argv[])
--- src/test_client.c
+++ src/test_client.c
@@ -5,6 +5,10 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
+#ifdef SOCK_UDP
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#endif
 
 #include "ws2811.h"
 #define ARRAY_SIZE(stuff)  (sizeof(stuff) / sizeof(stuff[0]))
@@ -20,16 +24,29 @@ ws2811_led_t dotcolors[] = {
     0x00200010,
 };
 
+#ifndef SOCK_UDP
 const char* socket_path = "./ledstrip.sock";
+#endif
 ws2811_led_t user_data[128];
 char buf_sock[512];
 int main(int argc, char *argv[]) {
-    struct sockaddr_un addr;
-    int sockdf, rr;
+#ifndef SOCK_UDP
+    struct sockaddr_un
+#else
+    struct sockaddr_in
+#endif
+        addr;
+    int sockfd, rr;
     int user_data_count = 0;
     if (argc > 1) {
         int ct = 0, tmp;
-        for (int i = 1; i < argc && i < ARRAY_SIZE(user_data); ++i) {
+        int i =
+#ifndef SOCK_UDP
+            1;
+#else
+            2; // argv[1] is reserved for the server IP
+#endif
+        for (; i < argc && i < ARRAY_SIZE(user_data); ++i) {
             if(sscanf(argv[i], "%i", &tmp) == 1) {
                 if (tmp >= 0 && tmp < ARRAY_SIZE(dotcolors)) {
                     user_data[ct++] = dotcolors[tmp];
@@ -42,34 +59,57 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Require integer arguments. Value range is [0-7].\n");
         exit(EXIT_FAILURE);
     }
-    sockdf = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (-1 == sockdf) {
-        perror("socket");
+    sockfd = socket(
+#ifndef SOCK_UDP
+        AF_UNIX, SOCK_STREAM,
+#else
+        AF_INET, SOCK_DGRAM,
+#endif
+    0);
+    if (-1 == sockfd) {
+        perror("ERROR opening socket");
         exit(EXIT_FAILURE);
     }
-    memset(&addr, 0, sizeof(struct sockaddr_un));
+    memset(&addr, 0, sizeof(addr));
+#ifndef SOCK_UDP
     addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path) - 1);
-    rr = connect(sockdf, (const struct sockaddr *) &addr,
-                   sizeof(struct sockaddr_un));
+    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+#else
+    addr.sin_family      = AF_INET;  // IPv4
+    addr.sin_port        = htons(5151);
+    if(1 != inet_pton(AF_INET, argv[1], &addr.sin_addr.s_addr)) {
+        perror("ERROR invalid ip addr");
+        exit(EXIT_FAILURE);
+    }
+#endif
+
+#ifndef SOCK_UDP
+    rr = connect(sockdf, (const struct sockaddr *)&addr, sizeof(addr));
     if (-1 == rr) {
         perror("ERROR socket connect");
         exit(EXIT_FAILURE);
     }
+#endif
     buf_sock[0] = '\xCC';
     buf_sock[1] = '\xCC';
     buf_sock[2] = (char)(user_data_count & 0xFF);
     buf_sock[3] = (char)((user_data_count >> 8) & 0xFF);
+#ifndef SOCK_UDP
     rr = write(sockdf, buf_sock, 4);
     if (-1 == rr) {
         perror("ERROR socket write");
         exit(EXIT_FAILURE);
     }
     rr = write(sockdf, user_data, user_data_count * sizeof(ws2811_led_t));
+#else
+    memcpy(buf_sock+4, user_data, user_data_count * sizeof(ws2811_led_t));
+    rr = sendto(sockfd, buf_sock, 4 + user_data_count * sizeof(ws2811_led_t),
+        MSG_CONFIRM, (const struct sockaddr *)&addr, sizeof(addr));
+#endif
     if (-1 == rr) {
         perror("ERROR socket write");
         exit(EXIT_FAILURE);
     }
-    close(sockdf);
+    close(sockfd);
     return 0;
 }
